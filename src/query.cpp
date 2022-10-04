e49b0116341782f76658b776a89ce10300a29c9d
Giulio Ermanno Pibiri <jeis90@gmail.com>
2022-10-04 18:29:33 +0200

    stricter test

--- src/query.cpp
+++ src/query.cpp
@@ -42,7 +42,7 @@ int main(int argc, char* argv[]) {
     }
     seq = kseq_init(fp);
     t.start();
-    while(kseq_read(seq) >= 0) {
+    while (kseq_read(seq) >= 0) {
         auto n = hf.barebone_streaming_query(seq->seq.s, seq->seq.l, canonical);
         total_kmers += n;
     }
@@ -59,7 +59,7 @@ int main(int argc, char* argv[]) {
     }
     seq = kseq_init(fp);
     t.start();
-    while(kseq_read(seq) >= 0) {
+    while (kseq_read(seq) >= 0) {
         auto n = hf.barebone_dumb_query(seq->seq.s, seq->seq.l, canonical);
         total_dumb_kmers += n;
     }
@@ -76,9 +76,10 @@ int main(int argc, char* argv[]) {
     }
     seq = kseq_init(fp);
     t.start();
-    while(kseq_read(seq) >= 0) {
+    while (kseq_read(seq) >= 0) {
         auto hashes = hf(seq->seq.s, seq->seq.l, canonical);
         total_aggregated_kmers += hashes.size();
+        essentials::do_not_optimize_away(hashes.data());
     }
     t.stop();
     if (seq) kseq_destroy(seq);
@@ -93,30 +94,27 @@ int main(int argc, char* argv[]) {
     }
     seq = kseq_init(fp);
     t.start();
-    while(kseq_read(seq) >= 0) {
+    while (kseq_read(seq) >= 0) {
         auto hashes = hf(seq->seq.s, seq->seq.l, canonical, false);
         total_aggregated_dumb_kmers += hashes.size();
+        essentials::do_not_optimize_away(hashes.data());
     }
     t.stop();
     if (seq) kseq_destroy(seq);
     gzclose(fp);
     total_aggregated_dumb_time = t.elapsed();
-    
-    std::cout << input_filename << 
-    "," << mphf_filename << 
-    "," << total_kmers << 
-    "," << static_cast<double>(total_time * 1000) / total_kmers << 
-    "," << static_cast<double>(total_dumb_time * 1000) / total_dumb_kmers << 
-    "," << static_cast<double>(total_aggregated_time * 1000) / total_aggregated_kmers <<
-    "," << static_cast<double>(total_aggregated_dumb_time * 1000) / total_aggregated_dumb_kmers << 
-    std::endl;
 
-    std::cerr << "number of k-mers:" <<
-    " " << total_kmers <<
-    " " << total_dumb_kmers << 
-    " " << total_aggregated_kmers << 
-    " " << total_aggregated_dumb_kmers << 
-    std::endl;
+    std::cout << input_filename << "," << mphf_filename << "," << total_kmers << ","
+              << static_cast<double>(total_time * 1000) / total_kmers << ","
+              << static_cast<double>(total_dumb_time * 1000) / total_dumb_kmers << ","
+              << static_cast<double>(total_aggregated_time * 1000) / total_aggregated_kmers << ","
+              << static_cast<double>(total_aggregated_dumb_time * 1000) /
+                     total_aggregated_dumb_kmers
+              << std::endl;
+
+    std::cerr << "number of k-mers:"
+              << " " << total_kmers << " " << total_dumb_kmers << " " << total_aggregated_kmers
+              << " " << total_aggregated_dumb_kmers << std::endl;
 }
 
 /*
